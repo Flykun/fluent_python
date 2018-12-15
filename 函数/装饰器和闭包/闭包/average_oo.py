f1bdee59059749e3adf26e45ae3ea15d1f238db1
Flykun <1689890728@qq.com>
2018-12-16 01:10:26 +0800

    更新

--- 函数/装饰器和闭包/闭包/average_oo.py
+++ 函数/装饰器和闭包/闭包/average_oo.py
@@ -12,3 +12,14 @@ class Averager:
         self.series.append(new_value)
         total = sum(self.series)
         return total / len(self.series)
+
+'''
+>>> avg = Averager()
+>>> avg(10)
+10.0
+>>> avg(11)
+10.5
+>>> avg(12)
+11.0
+>>>
+'''
